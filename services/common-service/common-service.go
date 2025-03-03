087ad57830301739e970161d7b8d81df1a5dc211
anchel <anchel.xy@gmail.com>
2025-03-03 11:03:42 +0800

    feat: 优化获取公网ip

--- services/common-service/common-service.go
+++ services/common-service/common-service.go
@@ -1,14 +1,29 @@
 package commonservice
 
-import "github.com/anchel/wechat-official-account-admin/lib/utils"
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+)
 
 func GetPublicIP() (string, error) {
-	restyClient := utils.GetRestyClient()
+	// restyClient := utils.GetRestyClient()
+	restyClient := resty.New()
+	// 设置超时时间
+	restyClient.SetTimeout(4 * time.Second)
+	// 设置代理
+	wxProxy := os.Getenv("WA_PROXY")
+	if wxProxy != "" {
+		restyClient.SetProxy(wxProxy)
+	}
 
 	req := restyClient.NewRequest()
-	resp, err := req.Get("https://ipinfo.io/ip")
+	resp, err := req.Get("https://ifconfig.me/ip")
 	if err != nil {
-		return "", err
+		log.Println("GetPublicIP", "err", err)
+		return "", nil
 	}
 
 	return string(resp.Body()), nil
